5223144cdc1db623529b7a5e7c9150916032f54a
jic.amaro@gmail.com <jic.amaro@gmail.com@b3f67564-1aec-9043-cab4-ccc1100fcd57>
2012-06-07 14:49:11 +0000

    asd

--- GuiaMovil/src/guia/movil/app/CommentWriteActivity.java
+++ GuiaMovil/src/guia/movil/app/CommentWriteActivity.java
@@ -9,11 +9,12 @@ import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 
 public class CommentWriteActivity extends Activity implements OnClickListener {
 	
-	private Button send;
-	private Button cancel;
+	private ImageButton send;
+	private ImageButton cancel;
 	private EditText nick;
 	private EditText comment;
 	@Override
@@ -22,25 +23,23 @@ public class CommentWriteActivity extends Activity implements OnClickListener {
 		 requestWindowFeature(Window.FEATURE_NO_TITLE);
 	     this.setContentView(R.layout.commentnew);
 	     getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-	     send =(Button) findViewById(R.id.sendComment);
-	     cancel =(Button) findViewById(R.id.cancelComment);
+	     send =(ImageButton) this.findViewById(R.id.comment_button);
+	     cancel =(ImageButton) this.findViewById(R.id.cancel_button);
 	     nick= (EditText) findViewById(R.id.nickText);
 	     comment= (EditText) findViewById(R.id.commentText);
 	     cancel.setOnClickListener(this);
 	     send.setOnClickListener(this);
-	     
-	     
 	 }
 	@Override
 	public void onClick(View v) {
 		// TODO Auto-generated method stub
-		if(v.getId() == R.id.cancelComment)
+		if(v.getId() == R.id.cancel_button)
 		{
 			Intent intent = new Intent(CommentWriteActivity.this,CommentsActivity.class);
 			finish();
 			this.startActivity(intent);
 		}
-		else if(v.getId()== R.id.sendComment)
+		else if(v.getId()== R.id.comment_button)
 		{
 			
 			 String methodname = "addComment";
